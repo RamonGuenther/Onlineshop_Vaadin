bf741cc2d3d63f7672a4e1dd76429e84a284a8af
Ivonne Kneißig <kneissig.ivonne@fh-swf.de>
2021-12-21 19:31:19 +0100

    Projektinitialisierung

---
+++ src/main/java/de/fhswf/in/fit/onlineshop/fitonlineshop/FitOnlineshopApplication.java
@@ -0,0 +1,13 @@
+package de.fhswf.in.fit.onlineshop.fitonlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitOnlineshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitOnlineshopApplication.class, args);
+	}
+
+}
